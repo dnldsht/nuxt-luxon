fb0e4317878b4d860ea88f984ee77beb7ff0dbd8
Donald Shtjefni <dnld.sht@gmail.com>
2025-10-25 17:59:02 +0200

    fix: types MaybeRefOrGetter

--- src/runtime/core/index.ts
+++ src/runtime/core/index.ts
@@ -5,22 +5,22 @@ import parseInput from './parse'
 import { extendInput, extendOutput } from './utils'
 
 export function makeParser(luxOptions: Required<LuxonOptions>) {
-  return (value: ParseInput, format?: FormatInputOptions) => parseInput(extendInput(luxOptions, value, format))
+  return (value: MaybeRefOrGetter<ParseInput>, inputFormat?: MaybeRefOrGetter<FormatInputOptions>) => parseInput(extendInput(luxOptions, toValue(value), toValue(inputFormat)))
 }
 
 export function makeFormatter(luxOptions: Required<LuxonOptions>) {
   const parser = makeParser(luxOptions)
-  return (value: ParseInput, format?: FormatOutputOptions, inputFormat?: FormatInputOptions) => {
+  return (value: MaybeRefOrGetter<ParseInput>, outputFormat?: MaybeRefOrGetter<FormatOutputOptions>, inputFormat?: MaybeRefOrGetter<FormatInputOptions>) => {
     const dt = parser(value, inputFormat)
-    return formatDateTime(dt, extendOutput(luxOptions, format))
+    return formatDateTime(dt, extendOutput(luxOptions, toValue(outputFormat)))
   }
 }
 
 export function makeReactiveParser(options: Required<LuxonOptions>) {
   const parse = makeParser(options)
 
-  return (value: MaybeRefOrGetter<ParseInput>, inputFormat?: FormatInputOptions) => {
-    return computed(() => parse(toValue(value), toValue(inputFormat)))
+  return (value: MaybeRefOrGetter<ParseInput>, inputFormat?: MaybeRefOrGetter<FormatInputOptions>) => {
+    return computed(() => parse(value, inputFormat))
   }
 }
 
@@ -28,6 +28,6 @@ export function makeReactiveFormatter(options: Required<LuxonOptions>) {
   const format = makeFormatter(options)
 
   return (value: MaybeRefOrGetter<ParseInput>, outputFormat?: MaybeRefOrGetter<FormatOutputOptions>, inputFormat?: MaybeRefOrGetter<FormatInputOptions>) => {
-    return computed(() => format(toValue(value), toValue(outputFormat), toValue(inputFormat)))
+    return computed(() => format(value, outputFormat, inputFormat))
   }
 }
